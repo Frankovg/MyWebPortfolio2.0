77dd13e25bf2f7f5b3bd7d2db53f89b3df0e1817
FranF3 <franco.amoroso@fathom3dev.com>
2024-08-09 20:18:40 +0200

    fixed the flex wrap in the mobile top menu

--- src/components/mobile-menu.tsx
+++ src/components/mobile-menu.tsx
@@ -13,13 +13,13 @@ type MobileMenuProps = {
 
 function MobileMenu({ open, close }: MobileMenuProps) {
   const isMobile = useMediaQuery("(max-width: 640px)")
-  const linkStyles = "transition-colors duration-300 ease-in-out hover:text-white"
+  const linkStyles = "whitespace-nowrap transition-colors duration-300 ease-in-out hover:text-white"
 
   return (
     <Sheet open={open} onOpenChange={close}>
-      <SheetContent side={isMobile ? 'right' : 'top'} className="max-[550px]:w-screen [&_svg]:w-6 [&_svg]:h-6 focus:[&_button]:ring-softGrey max-[550px]:border-none border-primary pt-[10vh] min-[550px]:pt-12 px-8 pb-8 bg-background">
+      <SheetContent side={isMobile ? 'right' : 'top'} className="max-[640px]:w-screen [&_svg]:w-6 [&_svg]:h-6 focus:[&_button]:ring-softGrey max-[640px]:border-none border-primary pt-[10vh] min-[640px]:pt-12 px-8 pb-8 bg-background">
         <div className="w-full h-full overflow-y-auto flex flex-col justify-between">
-          <ul className="w-full h-fit text-right min-[550px]:text-left max-[550px]:space-y-5 text-2xl flex max-[550px]:flex-col flex-row min-[550px]:gap-6 min-[550px]:items-center min-[550px]:justify-center flex-grow">
+          <ul className="w-full h-fit text-right min-[640px]:text-left max-[640px]:space-y-5 text-2xl flex max-[640px]:flex-col flex-row min-[640px]:gap-6 min-[640px]:items-center min-[640px]:justify-center flex-wrap">
             {ROUTES.map((route) => (
               <li key={route.path}>
                 <Link href={route.path} onClick={close} className={linkStyles}>
@@ -30,7 +30,7 @@ function MobileMenu({ open, close }: MobileMenuProps) {
             <li>
               <Link href='/#contact' className={linkStyles}>Contact</Link>
             </li>
-            <li><div className="max-[550px]:border-b min-[550px]:border-r min-[550px]:h-8 border-solid border-primary" /></li>
+            <li><div className="max-[640px]:border-b min-[640px]:border-r min-[640px]:h-8 border-solid border-primary" /></li>
             {SOCIAL.map((socialIcon) => (
               <li key={socialIcon.href}>
                 <Link href={socialIcon.href} className={linkStyles}>
@@ -39,8 +39,8 @@ function MobileMenu({ open, close }: MobileMenuProps) {
               </li>
             ))}
           </ul>
-          <div className="flex flex-col min-[550px]:flex-row gap-6 w-full h-full items-end justify-center min-[550px]:pt-16">
-            <h2 className="text-2xl min-[550px]:hidden">Downloads</h2>
+          <div className="flex flex-col min-[640px]:flex-row gap-6 w-full h-full items-end justify-center min-[640px]:pt-16">
+            <h2 className="text-2xl min-[640px]:hidden">Downloads</h2>
             <DownloadLinks />
           </div>
         </div>
