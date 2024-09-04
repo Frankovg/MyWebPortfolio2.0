fbdd4bc72691d0d338536c3e93d7571e8ab6f6dd
FranF3 <franco.amoroso@fathom3dev.com>
2024-09-04 14:21:34 +0200

    added prisma db

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePattern: [
+      {
+        protocol: "https",
+        hostname: "drive.google.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
