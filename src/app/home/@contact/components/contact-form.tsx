35e894dedfc6151a128309cfcbe885060f9e6f66
FranF3 <franco.amoroso@fathom3dev.com>
2024-10-16 08:22:55 +0200

    contact form done

--- src/app/home/@contact/components/contact-form.tsx
+++ src/app/home/@contact/components/contact-form.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { useTransition } from "react"
+
 //Components
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
@@ -19,49 +21,60 @@ import { contactFormSchema, TContactForm } from "@/lib/validations"
 //Actions
 import { sendMail } from "@/actions/actions"
 
-//TODO: agregar spinner al boton
-//TODO: borrar inputs al enviar correctamente
-//TODO: cambiar estilos del toast
-//TODO: testear inputs con datos erroneos y verificar mensajes de error
-//TODO: probar distintos formatos de telefonos
-
 function ContactForm() {
+  const [isPending, startTransition] = useTransition()
+
   const {
     register,
     trigger,
     getValues,
-    formState: { errors, isSubmitting },
+    formState: { errors },
+    reset,
   } = useForm<TContactForm>({
     resolver: zodResolver(contactFormSchema),
     defaultValues: undefined,
   })
 
+  const handleAction = async () => {
+    startTransition(async () => {
+      const result = await trigger()
+      if (!result) return
+
+      const messageData = getValues()
+      const mailText = `Name: ${messageData.first_name} ${messageData.last_name}\n  Email: ${messageData.email}\n Message: ${messageData.message}`
+      const mail = {
+        email: messageData.email,
+        subject: `Mensaje de ${messageData.email}`,
+        text: mailText,
+      }
+      const response = await sendMail(mail)
+
+      if (response?.messageId) {
+        reset()
+        toast.success('Application submitted successfully')
+      } else {
+        toast.error('Failed to send application', {
+          richColors: true
+        })
+      }
+    })
+  }
+
   return (
     <div className="w-full pb-24">
-      <Toaster />
-      <form
-        className="max-w-[645px] mx-auto"
-        action={async () => {
-          const result = await trigger()
-          if (!result) return
-
-          const messageData = getValues()
-          const mailText = `Name: ${messageData.first_name} ${messageData.last_name}\n  Email: ${messageData.email}\n Message: ${messageData.message}`
-          const mail = {
-            email: messageData.email,
-            subject: `Mensaje de ${messageData.email}`,
-            text: mailText,
-          }
-          const response = await sendMail(mail)
-
-          if (response?.messageId) {
-            toast.success('Application submitted successfully')
-          } else {
-            toast.error('Failed to send application', {
-              richColors: true
-            })
+      <Toaster
+        toastOptions={{
+          classNames: {
+            toast: 'bg-background',
+            title: 'text-whiteText',
+            success: 'text-success',
+            error: 'text-error',
           }
         }}
+      />
+      <form
+        className="max-w-[645px] mx-auto"
+        action={handleAction}
       >
         <div className="space-y-4">
           <div className="flex flex-col md:flex-row gap-3">
@@ -71,12 +84,11 @@ function ContactForm() {
                 className="bg-transparent"
                 id="first_name"
                 placeholder="John"
-                // maxLength={100}
                 {...register('first_name')}
               />
               {
                 errors.first_name &&
-                <span className='text-red-500'>{errors.first_name.message}</span>
+                <span className='text-error'>{errors.first_name.message}</span>
               }
             </div>
             <div className='space-y-1 w-full'>
@@ -85,12 +97,11 @@ function ContactForm() {
                 className="bg-transparent"
                 id="last_name"
                 placeholder="Doe"
-                // maxLength={100}
                 {...register('last_name')}
               />
               {
                 errors.last_name &&
-                <span className='text-red-500'>{errors.last_name.message}</span>
+                <span className='text-error'>{errors.last_name.message}</span>
               }
             </div>
           </div>
@@ -103,12 +114,11 @@ function ContactForm() {
                 type="tel"
                 id="phone"
                 placeholder="+34 608 222 555"
-                // maxLength={100}
                 {...register('phone')}
               />
               {
                 errors.phone &&
-                <span className='text-red-500'>{errors.phone.message}</span>
+                <span className='text-error'>{errors.phone.message}</span>
               }
             </div>
             <div className='space-y-1 w-full'>
@@ -118,13 +128,11 @@ function ContactForm() {
                 type="email"
                 id="email"
                 placeholder="john.doe@email.com"
-                // required
-                // maxLength={100}
                 {...register('email')}
               />
               {
                 errors.email &&
-                <span className='text-red-500'>{errors.email.message}</span>
+                <span className='text-error'>{errors.email.message}</span>
               }
             </div>
           </div>
@@ -142,16 +150,16 @@ function ContactForm() {
             </p>
             {
               errors.message &&
-              <span className='text-red-500'>{errors.message.message}</span>
+              <span className='text-error'>{errors.message.message}</span>
             }
           </div>
 
           <Button
             className="group relative bg-white text-lg font-semibold py-[18px] px-[34px] overflow-hidden w-full md:w-fit"
-            disabled={isSubmitting}
+            disabled={isPending}
           >
             <span className="absolute inset-0 bg-primary transform w-0 transition-all duration-300 ease-in-out group-hover:w-full" />
-            <span className="relative z-10 text-darkGrey">{isSubmitting ? 'Sending...' : 'Send'}</span>
+            <span className="relative z-10 text-darkGrey">{isPending ? 'Sending...' : 'Send'}</span>
           </Button>
         </div>
       </form>
--- src/lib/validations.ts
+++ src/lib/validations.ts
@@ -1,21 +1,21 @@
 import { z } from 'zod'
 
-const invalid_type_error = 'Invalid type provided for this field'
-const required_error = 'This field cannot be blank'
-const value_too_short_error = 'This field is too short'
-const value_too_long_error = 'This field must not be greater than 50 characters'
+const invalid_type_error = 'Invalid type provided for this field.'
+const required_error = 'This field cannot be blank.'
+const value_too_short_error = 'This field is too short.'
+const value_too_long_error = 'This field must not be greater than 50 characters.'
 
 const phoneValidation = new RegExp(
-  /^[+]{1}(?:[0-9\-\\(\\)\\/.]\s?){6,15}[0-9]{1}$/
+  /^(?:\+?\d{1,3})?[\s.-]?\(?\d{1,4}\)?[\s.-]?\d{1,4}[\s.-]?\d{1,4}[\s.-]?\d{1,4}$/
 )
 
 export const contactFormSchema = z.object({
   first_name: z.string({ invalid_type_error, required_error }).trim().min(1, { message: value_too_short_error }).max(50, value_too_long_error),
   last_name: z.string({ invalid_type_error, required_error }).trim().min(1, { message: value_too_short_error }).max(50, value_too_long_error),
-  phone: z.string().min(6, { message: value_too_short_error }).max(16).regex(phoneValidation, { message: 'Invalid phone' }).optional().or(z.literal('')),
+  phone: z.string().min(6, { message: value_too_short_error }).max(16).regex(phoneValidation, { message: 'Invalid phone.' }).optional().or(z.literal('')),
   email: z.string({ invalid_type_error, required_error }).email({ message: 'Please enter a valid email address.' }).min(1, { message: value_too_short_error }),
   message: z.string().trim().min(10, { message: 'Please make sure your message is at least 10 characters long.' }).max(1000),
-})
+}).strict()
 
 export type TContactForm = z.infer<typeof contactFormSchema>
 
