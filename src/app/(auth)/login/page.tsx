3da8548548f9595eb73570b4136fb5f9fd6cd5eb
FranF3 <franco.amoroso@fathom3dev.com>
2024-11-11 10:43:28 +0100

    new architecture done

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <main>
+      <h1 className='text-center mb-5'>Log In</h1>
+
+      {/* <AuthForm type='logIn' /> */}
+
+
+    </main>
+  )
+}
+
+export default Login
