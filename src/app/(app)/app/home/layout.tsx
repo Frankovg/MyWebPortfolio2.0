c1e8ee5964d030f2f47863b6bb720b6038cfc75f
FranF3 <franco.amoroso@fathom3dev.com>
2024-12-04 08:46:53 +0100

    sidebar inprovements

--- src/app/(app)/app/home/layout.tsx
+++ src/app/(app)/app/home/layout.tsx
@@ -1,19 +1,19 @@
-import Contact from "@/components/contact/page"
 import Main from "@/components/main"
 
 type LayoutProps = {
   children: React.ReactNode,
   techStack: React.ReactNode
   projects: React.ReactNode
+  contact: React.ReactNode
 }
 
-const Layout = ({ children, techStack, projects }: LayoutProps) => {
+const Layout = ({ children, techStack, projects, contact }: LayoutProps) => {
   return (
     <Main className='pt-[25vh]'>
       {children}
       {techStack}
       {projects}
-      <Contact />
+      {contact}
     </Main>
   )
 }
