532570fd38045f5d56b91e6de8684bd6d455163c
FranF3 <franco.amoroso@fathom3dev.com>
2025-02-27 08:11:13 +0100

    feat(project-page): full project page done

--- src/app/(app)/app/project/[slug]/components/project-pagination-card.tsx
+++ src/app/(app)/app/project/[slug]/components/project-pagination-card.tsx
@@ -1,18 +1,18 @@
-import { ArrowLeft } from "lucide-react"
-import Link from "next/link"
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 
-import ImageWithFallback from "@/components/image-with-fallback"
-import { FALLBACK_IMG, NEXT, PREV } from "@/lib/constants"
-import { PrevOrNextProject } from "@/lib/types"
-import { cn } from "@/lib/utils"
+import ImageWithFallback from "@/components/image-with-fallback";
+import { FALLBACK_IMG, NEXT, PREV } from "@/lib/constants";
+import { PrevOrNextProject } from "@/lib/types";
+import { cn } from "@/lib/utils";
 
 type ProjectPaginationCardProps = {
-  project: PrevOrNextProject
-  type: typeof PREV | typeof NEXT
-}
+  project: PrevOrNextProject;
+  type: typeof PREV | typeof NEXT;
+};
 
 function ProjectPaginationCard({ project, type }: ProjectPaginationCardProps) {
-  const isNext = (type === NEXT)
+  const isNext = type === NEXT;
 
   return (
     <Link
@@ -23,15 +23,17 @@ function ProjectPaginationCard({ project, type }: ProjectPaginationCardProps) {
       href={`/app/project/${project?.slug}`}
     >
       <div className="w-full p-2 max-550:flex flex-col-reverse 550:grid grid-cols-12 gap-8 550:gap-0 550:items-center">
-        <ArrowLeft className={cn(
-          "col-span-1 550:mx-auto max-550:w-10 h-auto",
-          isNext ? "ml-auto rotate-180 550:order-3" : "mr-auto"
-        )}
+        <ArrowLeft
+          className={cn(
+            "col-span-1 550:mx-auto max-550:w-10 h-auto",
+            isNext ? "ml-auto rotate-180 550:order-3" : "mr-auto"
+          )}
         />
-        <div className={cn(
-          "800:max-930:col-span-7 col-span-5 px-4 flex flex-col",
-          isNext && "550:order-2"
-        )}
+        <div
+          className={cn(
+            "800:max-930:col-span-7 col-span-5 px-4 flex flex-col",
+            isNext && "550:order-2"
+          )}
         >
           <p>{isNext ? "Next project" : "Previous project"}</p>
           <h2 className="font-bold text-xl">{project.title}</h2>
@@ -46,12 +48,12 @@ function ProjectPaginationCard({ project, type }: ProjectPaginationCardProps) {
           alt={project.title}
           width={0}
           height={0}
-          sizes={'100%'}
+          sizes={"100%"}
           quality={50}
         />
       </div>
     </Link>
-  )
+  );
 }
 
-export default ProjectPaginationCard
+export default ProjectPaginationCard;
