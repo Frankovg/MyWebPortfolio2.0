e45b8d6af206d2e84b547171e8de7da5dbbf83f7
FranF3 <franco.amoroso@fathom3dev.com>
2025-03-14 09:29:32 +0100

    feat(portfolio): added more columns

--- src/components/table/row-content-table.tsx
+++ src/components/table/row-content-table.tsx
@@ -21,7 +21,7 @@ function RowContentTable<T>({
   return (
     <TableRow
       data-state={row.getIsSelected() && "selected"}
-      className="hover:bg-black"
+      className="group/row hover:bg-black/30 transition-colors duration-200"
     >
       {row.getVisibleCells().map((cell) => (
         <TableCell key={cell.id} className="py-3">
