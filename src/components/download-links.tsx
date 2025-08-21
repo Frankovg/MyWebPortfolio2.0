e7bd4987b9d7c3647087fb65800d7b9fdf5f94c2
FranF3 <franco.amoroso@fathom3dev.com>
2025-08-21 08:00:47 +0200

    feat(downloads-menu): new design

--- src/components/download-links.tsx
+++ src/components/download-links.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { useUserDataContext } from "@/hooks/use-user-data-context";
+import H3 from "./primitives/h3";
 
 function DownloadLinks() {
   const { downloads } = useUserDataContext();
@@ -11,25 +12,31 @@ function DownloadLinks() {
   const parsedDownloads = downloads.filter((file) => file.isActive);
 
   return (
-    <>
+    <div className="flex flex-col gap-4 items-center w-max h-auto p-4">
       {parsedDownloads.map((download) => (
         <Link
           href={download.fileHref || "#"}
-          target="_blank"
-          className="group relative overflow-hidden"
+          target="_blan"
+          className="group relative overflow-hidden flex max-w-[400px] items-center"
           key={download.name}
         >
-          <Image
-            src={download.imageUrl}
-            alt={download.name}
-            width={200}
-            height={133}
-            className="w-[200px] h-[133px] object-cover transition-transform duration-300 ease-out group-hover:scale-[1.02]"
-          />
-          <div className="absolute top-0 left-0 w-full h-full bg-linear-to-r from-transparent via-primary/5 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-[2000ms] animate-shiny"></div>
+          <div className="w-2/5">
+            <Image
+              src={download.imageUrl}
+              alt={download.name}
+              width={200}
+              height={133}
+              className="w-[200px] h-[133px] object-cover transition-transform duration-300 ease-out group-hover:scale-[1.02]"
+            />
+            <div className="absolute top-0 left-0 w-full h-full bg-linear-to-r from-transparent via-primary/5 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-[2000ms] animate-shiny" />
+          </div>
+          <div className="w-3/5 px-5">
+            <h4 className="text-lg font-semibold group-hover:underline">{download.name}</h4>
+            <p className="text-md text-muted-foreground">{download.description}</p>
+          </div>
         </Link>
       ))}
-    </>
+    </div>
   );
 }
 
