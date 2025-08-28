106210fe1815cf9603e2f91ad4907f247c24a290
FranF3 <franco.amoroso@fathom3dev.com>
2025-08-28 08:13:17 +0200

    feat(skeleton): Hero loading

--- src/app/(app)/app/home/loading.tsx
+++ src/app/(app)/app/home/loading.tsx
@@ -1,7 +1,22 @@
+import React from 'react';
+
+import { Skeleton } from "@/components/ui/skeleton"
+
 export default function Loading() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <p>Loading...</p>
+    <div className='w-screen h-auto px-5'>
+      <section className="h-[50vh] pt-24">
+        <div className="w-full flex flex-col items-center space-y-0 550:space-y-3">
+          <Skeleton className='rounded-xl w-full max-w-[200px] h-4 mb-5 bg-softGrey' />
+          <Skeleton className='rounded-xl w-full max-w-[550px] h-[150px] bg-textAnimation' />
+          <div className='py-8 mb-3 w-full flex flex-col space-y-1.5 items-center'>
+            <Skeleton className='rounded-xl w-full max-w-[800px] h-5 bg-softGrey' />
+            <Skeleton className='rounded-xl w-full max-w-[720px] h-5 bg-softGrey' />
+          </div>
+          <Skeleton className='rounded-xl w-[200px] h-6 mb-3 bg-textAnimation' />
+        </div>
+      </section>
     </div>
   );
 }
+
--- src/app/(app)/app/home/page.tsx
+++ src/app/(app)/app/home/page.tsx
@@ -1,48 +1,13 @@
 import { Suspense } from "react";
 
-import AnimatedArrowRight from "@/components/primitives/animated-arrow-right";
-import H3 from "@/components/primitives/h3";
-import ScrollLink from "@/components/primitives/scroll-link";
-import Section from "@/components/section";
-
-import TextAnimation from "./components/text-animation";
+import { Hero } from "./components/hero";
 import Loading from "./loading";
-
-const toRotate = [
-  "Front-End Developer",
-  "UX/UI Designer",
-  "Industrial Designer",
-  "Web App Developer",
-];
 
 export default function Home() {
   return (
     <Suspense fallback={<Loading />}>
-      <Section id="home" className="h-[50vh] pt-24">
-        <div className="w-full text-center [&_h1]:text-white [&_h1]:text-3xl 550:[&_h1]:text-5xl 830:[&_h1]:text-7xl [&_h1]:font-semibold space-y-0 550:space-y-3">
-          <h4>Welcome to my portfolio</h4>
-          <h1>Hi! I&apos;m Franco</h1>
-          <TextAnimation texts={toRotate} />
-          <H3>
-            As a{" "}
-            <span className="font-semibold text-white">
-              Front-End Developer with a solid design background
-            </span>
-            , I bring a unique perspective to my work, creating web apps that
-            look great and feel intuitive. Check out my work, and let&apos;s
-            connect if you&apos;ve got questions or want to team up.
-          </H3>
-          <div className="w-fit mx-auto">
-            <ScrollLink
-              id="contact"
-              className="group text-white font-bold flex items-center text-lg"
-            >
-              <span>Let&apos;s connect</span>
-              <AnimatedArrowRight />
-            </ScrollLink>
-          </div>
-        </div>
-      </Section>
+      <Loading />
+      <Hero />
     </Suspense>
   );
 }
