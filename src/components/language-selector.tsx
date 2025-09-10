d338c8566e9970afa897adf456b26709ce6d338c
FranF3 <franco.amoroso@fathom3dev.com>
2025-09-10 16:45:50 +0200

    feat(language-selector): wrap language options in a div for better structure

--- src/components/language-selector.tsx
+++ src/components/language-selector.tsx
@@ -1,14 +1,14 @@
 function LanguageSelector() {
   //TODO: Add translations
   return (
-    <>
+    <div className="flex items-center space-x-1">
       <div>
         <span className="opacity-65">ES</span>
       </div>
       <div>
         <span>EN</span>
       </div>
-    </>
+    </div>
   )
 }
 
