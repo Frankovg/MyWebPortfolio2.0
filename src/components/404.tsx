59ed4f6ba6c4576a6f44e487febd5132193e734b
FranF3 <franco.amoroso@fathom3dev.com>
2025-09-11 15:37:08 +0200

    404 page

--- src/components/404.tsx
+++ src/components/404.tsx
@@ -1,9 +1,26 @@
-
+import Link from "next/link";
+import H3 from "./primitives/h3";
+import { FrownIcon } from "lucide-react";
 
 export const ErrorPage = async () => {
   return (
-    <div className="w-full h-screen flex flex-col items-start justify-center ">
-      Error page
+    <div className="absolute w-full h-full">
+      <div className="w-full h-full flex flex-col justify-center items-center">
+        <FrownIcon size={170} />
+        <H3 className="text-3xl font-semibold text-danger">404 - Page not found</H3>
+        <p className="text-3xl font-bold">
+          The link might be corrupted.
+        </p>
+        <p className="text-lg">or the page may have been removed</p>
+        <div className="my-10">
+          <Link
+            href="/"
+            className="uppercase p-3 border border-white"
+          >
+            go back home
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
