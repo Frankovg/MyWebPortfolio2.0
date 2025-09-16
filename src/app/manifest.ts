f453f942b6373c6aae35db2256f76455bb227345
FranF3 <franco.amoroso@fathom3dev.com>
2025-09-16 08:17:54 +0200

    feat: manifest

---
+++ src/app/manifest.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'FRAN Front-end Developer & Designer',
+    short_name: 'FRAN Portfolio',
+    description: 'Hi! This is my Web-Portfolio, I hope you like it!',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#1a1a1a',
+    theme_color: '#e453bc',
+  }
+}
