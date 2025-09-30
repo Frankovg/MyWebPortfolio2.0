eb2720a08d76722db0224ca13f71d174a4f11102
FranF3 <franco.amoroso@fathom3dev.com>
2025-09-30 07:54:57 +0200

    fixed a misspelling word

--- src/actions/contact-actions.ts
+++ src/actions/contact-actions.ts
@@ -9,7 +9,7 @@ import { emailSchema } from "@/lib/validations";
 const SMTP_SERVER_HOST = process.env.SMTP_SERVER_HOST;
 const SMTP_SERVER_USERNAME = process.env.SMTP_SERVER_USERNAME;
 const SMTP_SERVER_PASSWORD = process.env.SMTP_SERVER_PASSWORD;
-const SITE_MAIL_RECIEVER = process.env.SITE_MAIL_RECIEVER;
+const SITE_MAIL_RECEIVER = process.env.SITE_MAIL_RECEIVER;
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -59,13 +59,13 @@ export async function sendMail(mail: SendMailProps) {
   if (isVerified) {
     info = await transporter.sendMail({
       from: email,
-      to: sendTo || SITE_MAIL_RECIEVER,
+      to: sendTo || SITE_MAIL_RECEIVER,
       subject: subject,
       text: text,
       html: html ? html : "",
     });
     console.log("Message Sent", info.messageId);
-    console.log("Mail sent to", SITE_MAIL_RECIEVER);
+    console.log("Mail sent to", SITE_MAIL_RECEIVER);
   }
 
   return info;
