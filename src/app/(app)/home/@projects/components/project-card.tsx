0af1b39c199f23a8cabd2f60df5268c9fb292a2b
FranF3 <franco.amoroso@fathom3dev.com>
2025-11-07 09:08:32 +0100

    feat(project-card): new hovering init

--- src/app/(app)/home/@projects/components/project-card.tsx
+++ src/app/(app)/home/@projects/components/project-card.tsx
@@ -1,3 +1,4 @@
+import { PlayIcon } from "lucide-react";
 import Link from "next/link";
 
 import ImageWithFallback from "@/components/primitives/image-with-fallback";
@@ -7,13 +8,20 @@ import { ProjectCardProps } from "../types/types";
 
 function ProjectCard({ project }: ProjectCardProps) {
   const techStackString = project.techStack.map((tech) => tech.name).join(", ");
-
+  //TODO: Continue working here
   return (
     <Link
       href={`/project/${project.slug}`}
-      className="w-full h-full flex flex-col justify-between overflow-hidden p-2 rounded-sm shadow-background border border-solid border-darkPrimary transition-all duration-300 ease-in-out hover:bg-background hover:scale-[1.005] hover:shadow-lg"
+      className="
+        group w-full h-full flex flex-col justify-between overflow-hidden p-2 rounded-sm shadow-background 
+        border border-solid border-darkPrimary transition-all duration-300 ease-in-out 
+        hover:bg-background hover:scale-[1.005] hover:shadow-lg
+      "
     >
-      <div className="w-full min-h-62.5">
+      <div className="relative w-full min-h-62.5">
+        <div className="hidden group-hover:flex absolute top-2 right-3 items-center gap-2">
+          <PlayIcon />
+        </div>
         <ImageWithFallback
           className="size-full object-cover"
           src={project.image}
@@ -27,7 +35,11 @@ function ProjectCard({ project }: ProjectCardProps) {
       </div>
       <div className="w-full h-full px-6 py-4">
         <h2 className="font-bold text-xl mb-2">{project.title}</h2>
-        <p className="text-gray-600 text-base">{project.shortDescription}</p>
+        <p
+          className="opacity-50 text-base group-hover:opacity-100 transition-opacity duration-300 ease-in-out"
+        >
+          {project.shortDescription}
+        </p>
       </div>
       <div className="w-90/100 mx-auto border-t border-solid border-darkPrimary text-center pt-2.5 pb-1">
         <p className="text-xs text-primary/80">{techStackString}</p>
