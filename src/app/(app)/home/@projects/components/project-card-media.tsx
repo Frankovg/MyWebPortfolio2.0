2b2770943a9b505b82cebf2e3286fecf8eb9dc8d
FranF3 <franco.amoroso@fathom3dev.com>
2025-11-14 07:37:05 +0100

    fix(product-card): external links as buttons

--- src/app/(app)/home/@projects/components/project-card-media.tsx
+++ src/app/(app)/home/@projects/components/project-card-media.tsx
@@ -1,6 +1,10 @@
+'use client'
+
 import { ExternalLink, SquarePlayIcon } from "lucide-react"
+import { useMemo } from "react"
 
 import { GithubIcon } from "@/icons/social"
+
 
 type ProjectCardMediaProps = {
   website?: string | null,
@@ -9,41 +13,44 @@ type ProjectCardMediaProps = {
 }
 
 export const ProjectCardMedia = ({ website, repository, video }: ProjectCardMediaProps) => {
+
   const hasSome = website || repository || video
+  const buttons = useMemo(() => (
+    [
+      {
+        url: website,
+        icon: <GithubIcon className="size-6 fill-current" />
+      },
+      {
+        url: repository,
+        icon: <ExternalLink className="size-6" />
+      },
+      {
+        url: video,
+        icon: <SquarePlayIcon className="size-6" />
+      }
+    ]
+  ), [website, repository, video])
+
   if (hasSome) {
     return (
       <div className="hidden group-hover:flex absolute top-2 right-3 items-center gap-2 bg-background rounded-md p-1.5">
-        {repository &&
-          <a
-            href={repository}
-            target="_blank"
-            rel="noopener noreferrer"
-            onClick={(e) => e.stopPropagation()}
-            className="hover:bg-primary"
-          >
-            <GithubIcon className="size-6 fill-current" />
-          </a>
-        }
-        {website &&
-          <a
-            href={website}
-            target="_blank"
-            rel="noopener noreferrer"
-            onClick={(e) => e.stopPropagation()}
-          >
-            <ExternalLink className="size-6" />
-          </a>
-        }
-        {video &&
-          <a
-            href={video}
-            target="_blank"
-            rel="noopener noreferrer"
-            onClick={(e) => e.stopPropagation()}
-          >
-            <SquarePlayIcon className="size-6" />
-          </a>
-        }
+        {buttons.map((b) => {
+          if (b.url) {
+            return (
+              <button
+                key={b.url}
+                onClick={(e) => {
+                  e.stopPropagation()
+                  e.preventDefault()
+                  window.open(b.url ?? "", "_blank", "noopener,noreferrer")
+                }}
+              >
+                {b.icon}
+              </button>
+            )
+          }
+        })}
       </div>
     )
   }
