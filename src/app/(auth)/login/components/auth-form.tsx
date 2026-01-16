99acec0d9813b7f49c02eb0fe8937fd89f58341d
FranF3 <franco.amoroso@fathom3dev.com>
2026-01-16 08:31:25 +0100

    feat(login): show and hide password, styles and error handling

--- src/app/(auth)/login/components/auth-form.tsx
+++ src/app/(auth)/login/components/auth-form.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import React, { useActionState } from "react";
+import { useActionState } from "react";
 
 import { logIn } from "@/actions/index";
 
@@ -9,32 +9,35 @@ import InputLogin from "./input-login";
 import { SubmitButton } from "./submit-button";
 
 const AuthForm = () => {
-  const [logInError, dispatchLogIn, isPending] = useActionState(logIn, undefined);
-  const inputs = ["Email", "Password"];
+  const [logInState, dispatchLogIn, isPending] = useActionState(logIn, undefined);
 
   return (
     <form
       action={dispatchLogIn}
-      className="flex flex-col gap-y-4 md:gap-y-2 max-md:w-full max-md:px-4"
+      className="flex flex-col gap-y-4 md:gap-y-2 max-md:w-full w-80 max-md:px-4"
     >
-      {inputs.map((input) => {
-        const lowerCaseInput = input.toLowerCase();
-        return (
-          <InputLogin
-            key={lowerCaseInput}
-            label={input}
-            htmlFor={lowerCaseInput}
-            required
-            maxLength={100}
-            id={lowerCaseInput}
-            name={lowerCaseInput}
-            type={lowerCaseInput}
-          />
-        );
-      })}
+      <InputLogin
+        label="Email"
+        htmlFor="email"
+        required
+        maxLength={100}
+        id="email"
+        name="email"
+        type="email"
+        defaultValue={logInState?.email ?? ""}
+      />
+      <InputLogin
+        label="Password"
+        htmlFor="password"
+        required
+        maxLength={100}
+        id="password"
+        name="password"
+        type="password"
+      />
       <SubmitButton isPending={isPending} />
-      {logInError && (
-        <p className="text-red-500 text-sm mt-2">{logInError.message}</p>
+      {logInState?.message && (
+        <p className="text-red-500 text-sm mt-2">{logInState.message}</p>
       )}
       <Link href="/contact?request-demo-account=true" className='border rounded-md w-full flex justify-center py-2.5 mt-10 hover:text-white hover:border-white'>
         Request a demo account
